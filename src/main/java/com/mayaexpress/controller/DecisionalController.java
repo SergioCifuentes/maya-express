f6caafb24878f06a9f578d7839d6a63a48ea6e9f
BryanGmz <brgg13@gmail.com>
2023-11-18 00:31:19 -0600

    Added: Warehouse info qr history

--- src/main/java/com/mayaexpress/controller/DecisionalController.java
+++ src/main/java/com/mayaexpress/controller/DecisionalController.java
@@ -23,7 +23,7 @@ public class DecisionalController {
         this.decisionalService = decisionalService;
     }
 
-    //@PreAuthorize("hasAuthority('ADMIN') or hasAuthority('IT')")
+    @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('IT')")
     @PostMapping("/suggestion-regions-send")
     public ResponseEntity<List<PackagesByRegionDTO>> getSuggestionRegionsSend(@Valid @RequestBody DateDTO dateDTO) {
         try {
@@ -33,7 +33,7 @@ public class DecisionalController {
         }
     }
 
-    //@PreAuthorize("hasAuthority('ADMIN') or hasAuthority('IT')")
+    @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('IT')")
     @PostMapping("/suggestion-regions-receive")
     public ResponseEntity<List<PackagesByRegionDTO>> getSuggestionRegionsReceive(@Valid @RequestBody DateDTO dateDTO) {
         try {
@@ -43,7 +43,7 @@ public class DecisionalController {
         }
     }
 
-    //@PreAuthorize("hasAuthority('ADMIN') or hasAuthority('IT')")
+    @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('IT')")
     @PostMapping("/suggestion-department-send")
     public ResponseEntity<List<PackagesByDepartmentDTO>> getSuggestionDepartmentSend(@Valid @RequestBody DateDTO dateDTO) {
         try {
@@ -53,7 +53,7 @@ public class DecisionalController {
         }
     }
 
-    //@PreAuthorize("hasAuthority('ADMIN') or hasAuthority('IT')")
+    @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('IT')")
     @PostMapping("/suggestion-department-receive")
     public ResponseEntity<List<PackagesByDepartmentDTO>> getSuggestionDepartmentReceive(@Valid @RequestBody DateDTO dateDTO) {
         try {
@@ -63,6 +63,7 @@ public class DecisionalController {
         }
     }
 
+    @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('IT')")
     @PostMapping("/branches-suggestions-employees")
     public ResponseEntity<List<WarehouseDTO>> getBranchesSuggestionsEmployees(@Valid @RequestBody DateDTO dateDTO) {
         try {
@@ -72,6 +73,7 @@ public class DecisionalController {
         }
     }
 
+    @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('IT')")
     @PostMapping("/branches-suggestions-vehicles")
     public ResponseEntity<List<WarehouseDTO>> getBranchesSuggestionsVehicles(@Valid @RequestBody DateDTO dateDTO) {
         try {
